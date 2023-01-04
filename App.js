296cfed9b9336a6726f16ec258df79955a9070e1
AleksasN <aleksasne@gmail.com>
2023-01-04 20:35:10 +0200

    cmomiit

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from "react-native";
 
+const isAndroid = Platform.OS === "android";
+console.log(StatusBar.currentHeight);
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
+      <SafeAreaView style={{ flex: 1, marginTop: isAndroid ? 50 : 0 }}>
+        <View style={{ backgroundColor: "green", padding: 16 }}>
+          <Text>Belekas cia zodziu bus</Text>
+        </View>
+        <View style={{ flex: 1, backgroundColor: "blue", padding: 16 }}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
