79881948ba39d7056c499639b8e798e427200ef5
AleksasN <aleksasne@gmail.com>
2023-01-18 13:53:11 +0200

    ngrok installed

---
+++ functions/index.js
@@ -0,0 +1,13 @@
+const functions = require("firebase-functions");
+const { geocodeRequest } = require("./geocode");
+
+// // Create and deploy your first functions
+// // https://firebase.google.com/docs/functions/get-started
+//
+exports.geocode = functions.https.onRequest((request, response) => {
+  geocodeRequest(request, response);
+});
+
+// exports.placesNearby = functions.https.onRequest((request, response) => {
+//     placesRequest(request, response);
+//   });
--- src/services/location/location.service.js
+++ src/services/location/location.service.js
@@ -1,16 +1,24 @@
 import camelize from "camelize";
 
-import { locations } from "./location.mock";
+// import { locations } from "../../../functions/geocode/geocode.mock";
 
-export const locationRequest = (searchTerm) => {
-  return new Promise((resolve, reject) => {
-    const locationMock = locations[searchTerm];
-    if (!locationMock) {
-      reject("not found");
-    }
-    resolve(locationMock);
-  });
+export const locationRequest = async (searchTerm) => {
+  const res = await fetch(
+    `http://64a1-78-57-124-214.ngrok.io/mealstogo-aa8e1/us-central1/geocode?city=${searchTerm}`
+  );
+  // console.log(res.json());
+  return await res.json();
 };
+
+// export const locationRequest = (searchTerm) => {
+//   return new Promise((resolve, reject) => {
+//     const locationMock = locations[searchTerm];
+//     if (!locationMock) {
+//       reject("not found");
+//     }
+//     resolve(locationMock);
+//   });
+// };
 
 export const locationTransform = (result) => {
   const formattedResponse = camelize(result);
